44a21dac3eb489058e1043b6b77b6272b9978847
Jeff Orford <orphord@gmail.com>
2018-11-29 10:20:01 -0700

    Completed text to display to user. (#1)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and hammer t.
+            Edit <code>src/App.js</code> and hammer time MFer.
           </p>
           <a
             className="App-link"
